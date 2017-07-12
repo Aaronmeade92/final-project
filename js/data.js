c5de5b5fb0bd56882d30ade7c6eff8975a97bff1
Aaron <aaronmeade9292@gmail.com>
2017-07-11 20:43:13 -0700

    added tracking functions

--- js/data.js
+++ js/data.js
@@ -1,12 +1,13 @@
 'use strict';
-
 // localStorage.allButtonSelections = JSON.stringify(allSelections);
 
 // localStorage.allDrinkBac = JSON.stringify(drinkBac);
 
 // localStorage.allMeals = JSON.stringify(mealsConsumed);
 
-// localStorage.allDrinks = JSON.stringify(drinksConsumed);
+var totalBeers = 0;
+var totalWines = 0;
+var totalLiquor = 0;
 
 var allSelections = JSON.parse( localStorage.allButtonSelections);
 console.log(allSelections);
@@ -16,3 +17,23 @@ var drinkBac = JSON.parse( localStorage.allDrinkBac);
 var mealsConsumed = JSON.parse( localStorage.allMeals);
 
 var drinksConsumed = JSON.parse( localStorage.allDrinks);
+
+// functions for total BAC total drinks of each type and sub types
+
+
+for (var i = 0; i < drinksConsumed.length; i++) {
+  if(drinksConsumed[i] == 'pale' || drinksConsumed[i] == 'stout' || drinksConsumed[i] == 'ipa'){
+    totalBeers++;
+  }
+  else if (drinksConsumed[i] == 'red' || drinksConsumed[i] == 'white' || drinksConsumed[i] == 'bubbles' ) {
+    totalWines++;
+  }
+  else if (drinksConsumed[i] == 'neat' || drinksConsumed[i] == 'mixed' || drinksConsumed[i] == 'shot') {
+    totalLiquor++;
+  }
+
+}
+
+console.log(totalBeers + ' beer');
+console.log(totalLiquor + ' liquor');
+console.log(totalWines + ' wine');
