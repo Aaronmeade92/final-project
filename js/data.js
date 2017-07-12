caf4d764eacf48494ec1c553955c1e8b569d7331
Jedidiah Esposito <egerthe@gmail.com>
2017-07-11 21:09:24 -0700

    drunk blur

--- js/data.js
+++ js/data.js
@@ -1,4 +1,10 @@
 'use strict';
+
+$('#blurMe').foggy({
+  blurRadius: 2,          // In pixels.
+  opacity: 0.8,           // Falls back to a filter for IE.
+  cssFilterSupport: true  // Use "-webkit-filter" where available.
+});
 // localStorage.allButtonSelections = JSON.stringify(allSelections);
 
 // localStorage.allDrinkBac = JSON.stringify(drinkBac);
